ef17bbf0a0271dffa8c5c97299c952813137119e
Elivan Junior <elivanvljr@gmail.com>
2025-05-25 12:08:07 -0300

    Update recursivo3.c
    O maior valor de um array

--- Recursividade/recursivo3.c
+++ Recursividade/recursivo3.c
@@ -4,13 +4,13 @@
 #define TAMANHO_MAX 10
 
 int arrayMaior(int array[], int size){
-	if(size == 1){
+	if(size == 1){ //se o array so tiver 1 elemento, retorne esse elemento;
 		return array[0];
 	}
-	int maior = arrayMaior(array, size-1);
-	if(maior < array[size-1]){
+	
+	int maior = arrayMaior(array, size-1); //coloque a função recursiva dentro de uma variavel pra analise;
+	if(maior < array[size-1])
 		return array[size-1];
-	}
 	return maior;
 }
 
@@ -26,9 +26,8 @@ int main(){
 	
 	int vetor[10];
 	botaNoVetor(vetor, TAMANHO_MAX);
-	for(int i = 0; i < TAMANHO_MAX; i++){
+	for(int i = 0; i < TAMANHO_MAX; i++)
 		printf("[%i] ", vetor[i]);
-	}
 	printf("\n\nMaior do Array: %i ", arrayMaior(vetor, TAMANHO_MAX));
 	return 0;
 }
