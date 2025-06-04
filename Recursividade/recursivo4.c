9aed6a95bbaa952782bab8aa6276ca3bd059cddf
Elivan Junior <elivanvljr@gmail.com>
2025-06-04 08:29:27 -0300

    Update recursivo4.c

--- Recursividade/recursivo4.c
+++ Recursividade/recursivo4.c
@@ -13,9 +13,8 @@ void botaNoVetor(int array[], int size){
 int buscadorRecursivo(int array[], int tamanho, int chave){
 	if(tamanho == 0)
 		return -1;
-	if(array[tamanho - 1] == chave){
+	if(array[tamanho - 1] == chave)
 		return tamanho - 1;
-	}
 	return buscadorRecursivo(array, tamanho - 1, chave);
 }
 
@@ -23,9 +22,8 @@ int main(){
 	srand(time(NULL));
 	int lista[TAMANHO_MAX];
 	botaNoVetor(lista, TAMANHO_MAX);
-	for(int i = 0; i < TAMANHO_MAX; i++){
+	for(int i = 0; i < TAMANHO_MAX; i++)
 		printf("[%i] ", lista[i]);
-	}
 
 	int busca = buscadorRecursivo(lista, TAMANHO_MAX, 10);
 	if(busca != -1){
@@ -33,6 +31,5 @@ int main(){
 	}else{
 		printf("\nIt's over, não foi encontrado\n");
 	}
-	
 	return 0;
 }
